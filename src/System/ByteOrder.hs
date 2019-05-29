b1d1143d2a6eddb544d7abd7751ed46a1fc2cab4
Andrew Martin <andrew.thaddeus@gmail.com>
2019-05-29 11:32:18 -0400

    polish the implementation and write an example for the typed interface

--- src/System/ByteOrder.hs
+++ src/System/ByteOrder.hs
@@ -1,10 +1,80 @@
+{-# language DataKinds #-}
+{-# language ExplicitForAll #-}
+{-# language KindSignatures #-}
+{-# language ScopedTypeVariables #-}
+{-# language TypeApplications #-}
+
+{-| This module offers an interface to portably work with byte
+arrays whose contents are known to be of a fixed endianness.
+There are two ways to use this module:
+
+* Untyped Conversions: The functions 'toBigEndian', 'toLittleEndian',
+  'fromBigEndian', and 'fromLittleEndian' convert between native-endian
+  words and big/little-endian words. The word resulting from
+  @to(Big|Little)Endian@ should be written to a primitive byte
+  array or a pointer afterwards. (There is no other purpose of
+  such a conversion.) Similarly, the argument to @from(Big|Little)Endian@
+  should be a word that was read from a primitive byte array or
+  a pointer. This interface is useful when serializing or deserializing
+  a data structure with fields of varying sizes.
+* Typed Conversions: The type 'Fixed' provides a convenient
+  type-directed interface to working with arrays of homogenous words.
+  This interface is easier to use and should be preferred when
+  possible.
+
+Suppose there is a protocol for aggregating numbers that uses stream
+sockets for communication. The protocol interprets all numbers as
+unsigned. It is described as follows:
+
+1. The client sends the server a little-endian 16-bit number @N@.
+   This is the count of numbers that will follow.
+2. The client sends the server @N@ little-endian 64-bit numbers.
+3. The server responds with two 64-bit numbers: the sum and the
+   product.
+
+Assume the existence of a @send@ and @receive@ that block until
+the total number of requested bytes have been handled. Additionally,
+assume a @typed@ and @untyped@ function that convert between
+'PrimArray' and 'ByteArray' by changing out the data constructor.
+
+> send :: Socket -> ByteArray -> IO ()
+> receive :: Socket -> Int -> IO ByteArray
+> typed :: ByteArray -> PrimArray a
+> untyped :: PrimArray a -> ByteArray
+
+For simplicity, all error-handling is omitted. With the type-directed
+interface, the server is implemented as:
+
+> server :: Socket -> IO a
+> server sckt = forever $ do
+>   totalByteArray <- receive sckt 2
+>   let totalPrimArray = typed totalByteArray :: PrimArray (Fixed 'LittleEndian Word16)
+>   let Fixed total = indexPrimArray totalPrimArray 0
+>   numberByteArray <- receive sckt (8 * fromIntegral @Word16 @Int total)
+>   let (sum,prod) = foldlPrimArray'
+>         (\(!sumN,!prodN) (Fixed n) -> (sumN + n, prodN * n))
+>         (0,1)
+>         (typed numberByteArray :: PrimArray (Fixed 'LittleEndian Word64))
+>   reply :: MutablePrimArray RealWorld (Fixed 'LittleEndian Word64) <- newPrimArray 2
+>   writePrimArray reply 0 (Fixed sum)
+>   writePrimArray reply 1 (Fixed prod)
+>   send sckt . untyped =<< unsafeFreezePrimArray reply
+
+Not all of the explicit type annotations are needed, but they have been
+provided for additional clarity. As long as the user ensures that the
+typed primitive arrays use 'Fixed' in their element types, the endianness
+conversions are guaranteed to be correct.
+
+-}
 module System.ByteOrder
   ( -- * Types
     ByteOrder(..)
+  , Fixed(..)
     -- * System Byte Order
   , targetByteOrder
-    -- * Class
+    -- * Classes
   , Bytes
+  , FixedOrdering
     -- * Convert
   , toBigEndian
   , toLittleEndian
@@ -12,11 +82,16 @@ module System.ByteOrder
   , fromLittleEndian
   ) where
 
+import Data.Coerce (coerce)
+import Data.Kind (Type)
 import GHC.ByteOrder (ByteOrder(..),targetByteOrder)
 import System.ByteOrder.Class (Bytes(..))
+import System.ByteOrder.Unsafe (Fixed(Fixed),FixedOrdering,toFixedEndian)
 
+-- | Convert from a big-endian word to a native-endian word.
 fromBigEndian :: Bytes a => a -> a
 fromBigEndian = toBigEndian
 
+-- | Convert from a little-endian word to a native-endian word.
 fromLittleEndian :: Bytes a => a -> a
 fromLittleEndian = toLittleEndian
--- src/System/ByteOrder/Class.hs
+++ src/System/ByteOrder/Class.hs
@@ -1,3 +1,5 @@
+{-# language TypeApplications #-}
+
 module System.ByteOrder.Class
   ( Bytes(..)
   ) where
@@ -5,12 +7,21 @@ module System.ByteOrder.Class
 import GHC.ByteOrder (ByteOrder(..),targetByteOrder)
 import Data.Word (Word8,Word16,Word32,Word64)
 import Data.Word (byteSwap16,byteSwap32,byteSwap64)
+import Data.Int (Int8,Int16,Int32,Int64)
 
+-- | Types that are represented as a fixed-sized word. For these
+-- types, the bytes can be swapped. The instances of this class
+-- use byteswapping primitives and compile-time knowledge of native
+-- endianness to provide portable endianness conversion functions.
 class Bytes a where
+  -- | Convert from a native-endian word to a big-endian word.
   toBigEndian :: a -> a
+  -- | Convert from a native-endian word to a little-endian word.
   toLittleEndian :: a -> a
 
 instance Bytes Word8 where
+  {-# inline toBigEndian #-}
+  {-# inline toLittleEndian #-}
   toBigEndian = id
   toLittleEndian = id
 
@@ -42,4 +53,58 @@ instance Bytes Word64 where
     LittleEndian -> byteSwap64
   toLittleEndian = case targetByteOrder of
     BigEndian -> byteSwap64
+    LittleEndian -> id
+
+instance Bytes Int8 where
+  {-# inline toBigEndian #-}
+  {-# inline toLittleEndian #-}
+  toBigEndian = id
+  toLittleEndian = id
+
+instance Bytes Int16 where
+  {-# inline toBigEndian #-}
+  {-# inline toLittleEndian #-}
+  toBigEndian = case targetByteOrder of
+    BigEndian -> id
+    LittleEndian ->
+        fromIntegral @Word16 @Int16
+      . byteSwap16
+      . fromIntegral @Int16 @Word16
+  toLittleEndian = case targetByteOrder of
+    BigEndian ->
+        fromIntegral @Word16 @Int16
+      . byteSwap16
+      . fromIntegral @Int16 @Word16
+    LittleEndian -> id
+
+instance Bytes Int32 where
+  {-# inline toBigEndian #-}
+  {-# inline toLittleEndian #-}
+  toBigEndian = case targetByteOrder of
+    BigEndian -> id
+    LittleEndian ->
+        fromIntegral @Word32 @Int32
+      . byteSwap32
+      . fromIntegral @Int32 @Word32
+  toLittleEndian = case targetByteOrder of
+    BigEndian ->
+        fromIntegral @Word32 @Int32
+      . byteSwap32
+      . fromIntegral @Int32 @Word32
+    LittleEndian -> id
+
+instance Bytes Int64 where
+  {-# inline toBigEndian #-}
+  {-# inline toLittleEndian #-}
+  toBigEndian = case targetByteOrder of
+    BigEndian -> id
+    LittleEndian ->
+        fromIntegral @Word64 @Int64
+      . byteSwap64
+      . fromIntegral @Int64 @Word64
+  toLittleEndian = case targetByteOrder of
+    BigEndian ->
+        fromIntegral @Word64 @Int64
+      . byteSwap64
+      . fromIntegral @Int64 @Word64
     LittleEndian -> id
