b04e99f99b934557b980d34d47ab6caa4be3c00a
Andrew Martin <andrew.thaddeus@gmail.com>
2019-07-08 14:25:39 -0400

    document the BigEndian and LittleEndian byte array modules

--- src/Data/Primitive/ByteArray/BigEndian.hs
+++ src/Data/Primitive/ByteArray/BigEndian.hs
@@ -1,3 +1,7 @@
+-- | This is drop-in replacement for the read, write, and index functions
+-- present in @Data.Primitive.ByteArray@ and @Data.Primitive.ByteArray.Unaligned@.
+-- While the functions from those modules use native byte order, the functions
+-- in this one use big-endian byte order (most significant byte first).
 module Data.Primitive.ByteArray.BigEndian
   ( -- * Aligned
     writeByteArray
@@ -16,23 +20,41 @@ import Data.Primitive.ByteArray.Unaligned (PrimUnaligned)
 import qualified Data.Primitive as PM
 import qualified Data.Primitive.ByteArray.Unaligned as PMU
 
+-- | Write a primitive value to the byte array. The offset is given
+-- in elements of type @a@ rather than in bytes. The most significant
+-- byte in the value comes first.
 writeByteArray :: (PrimMonad m, Prim a, Bytes a) => MutableByteArray (PrimState m) -> Int -> a -> m ()
 writeByteArray arr ix v = PM.writeByteArray arr ix (toBigEndian v)
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the most significant one. The offset is given in elements of
+-- type @a@ rather than in bytes.
 readByteArray :: (PrimMonad m, Prim a, Bytes a) => MutableByteArray (PrimState m) -> Int -> m a
 readByteArray arr ix = fromBigEndian <$> PM.readByteArray arr ix
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the most significant one. The offset is given in elements of
+-- type @a@ rather than in bytes.
 indexByteArray :: (Prim a, Bytes a) => ByteArray -> Int -> a
 indexByteArray arr ix = fromBigEndian (PM.indexByteArray arr ix)
 
+-- | Write a primitive value to the byte array. The offset is given
+-- in bytes rather than in elements of type @a@. The most significant
+-- byte in the value comes first.
 writeUnalignedByteArray :: (PrimMonad m, PrimUnaligned a, Bytes a)
   => MutableByteArray (PrimState m) -> Int -> a -> m ()
 writeUnalignedByteArray arr ix v = PMU.writeUnalignedByteArray arr ix (toBigEndian v)
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the most significant one. The offset is given in bytes rather
+-- than in elements of type @a@.
 readUnalignedByteArray :: (PrimMonad m, PrimUnaligned a, Bytes a)
   => MutableByteArray (PrimState m) -> Int -> m a
 readUnalignedByteArray arr ix = fromBigEndian <$> PMU.readUnalignedByteArray arr ix
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the most significant one. The offset is given in bytes rather
+-- than in elements of type @a@.
 indexUnalignedByteArray :: (PrimUnaligned a, Bytes a)
   => ByteArray -> Int -> a
 indexUnalignedByteArray arr ix = fromBigEndian (PMU.indexUnalignedByteArray arr ix)
--- src/Data/Primitive/ByteArray/LittleEndian.hs
+++ src/Data/Primitive/ByteArray/LittleEndian.hs
@@ -1,3 +1,7 @@
+-- | This is drop-in replacement for the read, write, and index functions
+-- present in @Data.Primitive.ByteArray@ and @Data.Primitive.ByteArray.Unaligned@.
+-- While the functions from those modules use native byte order, the functions
+-- in this one use little-endian byte order (least significant byte first).
 module Data.Primitive.ByteArray.LittleEndian
   ( -- * Aligned
     writeByteArray
@@ -16,24 +20,43 @@ import Data.Primitive.ByteArray.Unaligned (PrimUnaligned)
 import qualified Data.Primitive as PM
 import qualified Data.Primitive.ByteArray.Unaligned as PMU
 
-writeByteArray :: (PrimMonad m, Prim a, Bytes a) => MutableByteArray (PrimState m) -> Int -> a -> m ()
+-- | Write a primitive value to the byte array. The offset is given
+-- in elements of type @a@ rather than in bytes. The least significant
+-- byte in the value comes first.
+writeByteArray :: (PrimMonad m, Prim a, Bytes a)
+  => MutableByteArray (PrimState m) -> Int -> a -> m ()
 writeByteArray arr ix v = PM.writeByteArray arr ix (toLittleEndian v)
 
-readByteArray :: (PrimMonad m, Prim a, Bytes a) => MutableByteArray (PrimState m) -> Int -> m a
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the least significant one. The offset is given in elements of
+-- type @a@ rather than in bytes.
+readByteArray :: (PrimMonad m, Prim a, Bytes a)
+  => MutableByteArray (PrimState m) -> Int -> m a
 readByteArray arr ix = fromLittleEndian <$> PM.readByteArray arr ix
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the least significant one. The offset is given in elements of
+-- type @a@ rather than in bytes.
 indexByteArray :: (Prim a, Bytes a) => ByteArray -> Int -> a
 indexByteArray arr ix = fromLittleEndian (PM.indexByteArray arr ix)
 
+-- | Write a primitive value to the byte array. The offset is given
+-- in bytes rather than in elements of type @a@. The least significant
+-- byte in the value comes first.
 writeUnalignedByteArray :: (PrimMonad m, PrimUnaligned a, Bytes a)
   => MutableByteArray (PrimState m) -> Int -> a -> m ()
 writeUnalignedByteArray arr ix v = PMU.writeUnalignedByteArray arr ix (toLittleEndian v)
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the least significant one. The offset is given in bytes rather
+-- than in elements of type @a@.
 readUnalignedByteArray :: (PrimMonad m, PrimUnaligned a, Bytes a)
   => MutableByteArray (PrimState m) -> Int -> m a
 readUnalignedByteArray arr ix = fromLittleEndian <$> PMU.readUnalignedByteArray arr ix
 
+-- | Read a primitive value from the byte array, interpreting the first
+-- byte as the least significant one. The offset is given in bytes rather
+-- than in elements of type @a@.
 indexUnalignedByteArray :: (PrimUnaligned a, Bytes a)
   => ByteArray -> Int -> a
 indexUnalignedByteArray arr ix = fromLittleEndian (PMU.indexUnalignedByteArray arr ix)
-
